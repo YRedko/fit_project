abed9eb2b04df1b5722655cdce565cc4c330585f
YRedko <wizardcattus@gmail.com>
2019-02-17 13:58:19 +0200

    fit_project domain

--- src/domain/Day.java
+++ src/domain/Day.java
@@ -5,7 +5,44 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Day {
+
     private Long id;
     private Instant date;
+    private User owner;
     private List<Food> food = new ArrayList<>();
+
+    public Day() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<Food> getFood() {
+        return food;
+    }
+
+    public void setFood(List<Food> food) {
+        this.food = food;
+    }
 }
