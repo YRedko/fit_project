37dcec0cef2ce355fe81955344f44a39f314fbcf
yredko <wizardcattus@gmail.com>
2019-05-10 02:31:22 +0300

    Merge branch 'master' of https://github.com/YRedko/fit_project
    # Conflicts:
    #	.idea/modules/fit_project_main.iml
    #	.idea/modules/fit_project_test.iml

--- src/main/java/project/FitProjectApp.java
+++ src/main/java/project/FitProjectApp.java
@@ -3,6 +3,7 @@ package project;
 import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import project.configuration.ApplicationFactory;
 import project.domain.Day;
@@ -12,7 +13,7 @@ import project.service.DayService;
 
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class FitProjectApp {
 
     public static void main(String[] args) {
@@ -20,6 +21,7 @@ public class FitProjectApp {
         //System.out.println("Hello World");
         ApplicationContext context = SpringApplication.run(FitProjectApp.class, args);
         DayService dayService = context.getBean(DayService.class);
+        User user1 = new User("l", "2");
         User user = new User("login", "password");
         System.out.println(user);
     }
